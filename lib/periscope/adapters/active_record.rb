e48af655c12c3d3ac808af9f8284920d84e85713
laserlemon <steve.richert@gmail.com>
2011-02-01 23:44:19 -0500

    Extract common functionality out of the ActiveRecord adapter into an abstract adapter module.

--- lib/periscope/adapters/active_record.rb
+++ lib/periscope/adapters/active_record.rb
@@ -1,65 +1,18 @@
-require 'active_support/core_ext/class/attribute.rb'
-require 'periscope/permission_set'
+require 'periscope/adapters/abstract'
 
 module Periscope
-  module ActiveRecord
-    extend ActiveSupport::Concern
+  module Adapters
+    module ActiveRecord
+      extend ActiveSupport::Concern
+      include Abstract
 
-    included do
-      class_attribute :_accessible_scopes
-      class_attribute :_protected_scopes
-      class_attribute :_periscope_authorizer
-    end
-
-    module ClassMethods
-      def periscope(params)
-        params.inject(scoped) do |chain, (key, value)|
-          accessible_scopes.include?(key) ? chain.send(key, value) : chain
+      module ClassMethods
+        def periscope(params)
+          params.inject(scoped) do |chain, (key, value)|
+            accessible_scopes.include?(key) ? chain.send(key, value) : chain
+          end
         end
       end
-
-      def scope_protected(*scopes)
-        self._protected_scopes = protected_scopes + scopes
-        self._periscope_authorizer = _protected_scopes
-      end
-
-      alias_method :down_periscope, :scope_protected
-
-      def scope_accessible(*scopes)
-        self._accessible_scopes = accessible_scopes + scopes
-        self._periscope_authorizer = _accessible_scopes
-      end
-
-      alias_method :up_periscope, :scope_accessible
-
-      def protected_scopes
-        self._protected_scopes ||= BlackList.new.tap do |list|
-          list.logger = logger if respond_to?(:logger)
-        end
-      end
-
-      def accessible_scopes
-        self._accessible_scopes ||= WhiteList.new(scopes_accessible_by_default).tap do |list|
-          list.logger = logger if respond_to?(:logger)
-        end
-      end
-
-      def periscope_authorizer
-        self._periscope_authorizer ||= accessible_scopes
-      end
-
-      def scopes_accessible_by_default
-        []
-      end
-
-      protected
-        def sanitize_for_search(params)
-          scope_authorizer.sanitize(params)
-        end
-
-        def search_authorizer
-          self.class.periscope_authorizer
-        end
     end
   end
 end
