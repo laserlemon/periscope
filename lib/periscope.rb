4e4f01471d4912700c056266c2df3b160dc5fe6c
Steve Richert <steve.richert@gmail.com>
2012-06-27 16:45:50 -0400

    Annihilate the lib and spec directories in order to start from scratch

--- lib/periscope.rb
+++
@@ -1,5 +0,0 @@
-require 'active_support'
-
-Dir[File.expand_path('../periscope/adapters/**/*.rb', __FILE__)].each{|f| require f }
-
-ActiveRecord::Base.send(:include, Periscope::Adapters::ActiveRecord) if defined?(ActiveRecord::Base)
