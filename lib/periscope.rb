38992d78373375f2f7ba90bb9e70fd447d06230e
Steve Richert <steve.richert@gmail.com>
2012-06-28 07:47:02 -0400

    Add stubbed-out, adapter-agnostic tests around scope chaining

--- lib/periscope.rb
+++ lib/periscope.rb
@@ -1,2 +1,22 @@
 module Periscope
+  def scope_accessible(*scopes)
+    options = scopes.last.is_a?(Hash) ? scopes.pop : {}
+    scopes.each{|s| periscope_options[s.to_s] = options }
+  end
+
+  def periscope(params = {})
+    params.inject(periscope_default_scope) do |chain, (scope, value)|
+      periscope_options.key?(scope.to_s) ? chain.send(scope, value) : chain
+    end
+  end
+
+  private
+
+  def periscope_options
+    @periscope_options ||= {}
+  end
+
+  def periscope_default_scope
+    where
+  end
 end
--- spec/periscope_spec.rb
+++ spec/periscope_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
 describe Periscope do
-  it 'is great'
+  let(:model) do
+    klass = mock
+    klass.extend(Periscope)
+    klass.stub(:periscope_default_scope => klass)
+    klass
+  end
+
+  def expect_scopes(hash)
+    hash.each do |scope, value|
+      model.should_receive(scope).once.with(value).and_return(model)
+    end
+  end
+
+  it 'uses the default scope for no params' do
+    scoped = mock
+    model.should_receive(:periscope_default_scope).once.and_return(scoped)
+    model.periscope.should == scoped
+  end
+
+  it 'uses the default scope for empty params' do
+    scoped = mock
+    model.should_receive(:periscope_default_scope).once.and_return(scoped)
+    model.periscope({}).should == scoped
+  end
+
+  it 'ignores protected scopes' do
+    model.should_not_receive(:foo)
+    model.periscope(:foo => 'bar')
+  end
+
+  it 'calls accessible scopes with values' do
+    expect_scopes(:foo => 'bar')
+    model.scope_accessible(:foo)
+    model.periscope(:foo => 'bar')
+  end
+
+  it 'recognizes accessible scopes, whether string or symbol' do
+    expect_scopes(:foo => 'baz', :bar => 'mitzvah')
+    model.scope_accessible(:foo, 'bar')
+    model.periscope('foo' => 'baz', :bar => 'mitzvah')
+  end
 end
