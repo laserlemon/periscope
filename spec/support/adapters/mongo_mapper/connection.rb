5ba9f888ccbbb8c8b4784e79682c941edbe00fe1
Steve Richert <steve.richert@gmail.com>
2014-06-24 10:24:36 -0400

    Fix intermittent MongoMapper test suite failures

--- spec/support/adapters/mongo_mapper/connection.rb
+++ spec/support/adapters/mongo_mapper/connection.rb
@@ -1,3 +1,12 @@
+# We need to require "active_support" first here because MongoMapper requires
+# "active_support/core_ext" without first requiring "active_support". On some
+# systems, this causes intermittent test suite failures.
+#
+# See: https://github.com/rails/rails/issues/14664
+# and: https://github.com/mongomapper/mongomapper/commit/64f9fc6
+#
+require "active_support"
+
 require "mongo_mapper"
 
 MongoMapper.connection = Mongo::Connection.new
