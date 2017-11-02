bb03abb276981b3b3dc9caa30e48bd2da46b2df3
Steve Richert <steve.richert@gmail.com>
2017-11-02 12:04:45 -0400

    Modify DatabaseCleaner configuration

--- spec/support/adapters/mongoid/database_cleaner.rb
+++ spec/support/adapters/mongoid/database_cleaner.rb
@@ -5,7 +5,15 @@ require "database_cleaner"
 DatabaseCleaner["mongoid"].strategy = :truncation
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
   config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
     DatabaseCleaner.clean
   end
 end
